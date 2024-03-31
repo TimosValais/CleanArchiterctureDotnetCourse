b01bab4d6678213a67d057c105ed309e95bb4f27
Timos Valais <timosbal@gmail.com>
2024-03-31 21:35:15 +0200

    initial commit

---
+++ src/GymManagement.Application/Services/SubscriptionsService.cs
@@ -0,0 +1,10 @@
+
+namespace GymManagement.Application.Services;
+
+public class SubscriptionService : ISubscriptionsService
+{
+    public Guid CreateSubscription(string subscriptionType, Guid adminId)
+    {
+        return Guid.NewGuid();
+    }
+}
