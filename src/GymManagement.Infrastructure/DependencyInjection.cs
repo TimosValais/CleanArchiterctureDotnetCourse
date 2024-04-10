8f9959797228f2123ed571ce97a545bb70f50e40
Timos Valais <timosbal@gmail.com>
2024-04-10 23:11:56 +0200

    fixed DI for GymsRepository

--- src/GymManagement.Infrastructure/DependencyInjection.cs
+++ src/GymManagement.Infrastructure/DependencyInjection.cs
@@ -3,16 +3,19 @@ using GymManagement.Infrastructure.Subscriptions.Persistence;
 using GymManagement.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using GymManagement.Infrastructure.Gyms.Persistence;
 
 namespace GymManagement.Infrastructure;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
         services.AddDbContext<GymManagementDbContext>(options =>
-            options.UseSqlite("Data Source = GymManagement.db")
-        );
+            options.UseSqlite("Data Source = GymManagement.db"));
+
+        // services.AddScoped<IAdminsRepository, AdminsRepository>();
+        services.AddScoped<IGymsRepository, GymsRepository>();
+        services.AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
         services.AddScoped<IUnitOfWork>(serviceProvider => serviceProvider.GetRequiredService<GymManagementDbContext>());
         return services;
     }
